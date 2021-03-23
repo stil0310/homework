ba55b6d4d0ee59222cc40a19caa5975e60235cd0
irina <panterka_0310@bk.ru>
2021-03-23 16:23:07 +0300

    Add homework3

---
+++ src/geekbrains/Fruit.java
@@ -0,0 +1,13 @@
+package geekbrains;
+
+public abstract class Fruit {
+    protected float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ src/geekbrains/Orange.java
@@ -0,0 +1,7 @@
+package geekbrains;
+
+public class Orange extends Fruit {
+    public Orange() {
+        super(1.5f);
+    }
+}
